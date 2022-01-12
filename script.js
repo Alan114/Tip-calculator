db70622916e705e0d019d715e42a52dea2c93043
Alan Pečenković <alanquad@yahoo.com>
2022-01-12 11:52:51 +0100

    added total value to formula and attached event listener to people input

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const tipPercentage = document.querySelectorAll(".number");
 const peopleInput = document.querySelector("#people");
 const tipAmount = document.querySelector("#tip-amount");
 const total = document.querySelector("#total");
+const reset = document.querySelector("#reset");
 
 billInput.addEventListener(
   "click",
@@ -15,6 +16,7 @@ billInput.addEventListener(
 billInput.addEventListener("input", (e) => {
   billInput.value = e.target.value;
   tipAmount.value = billInput.value;
+  total.value = billInput.value;
   console.log(e.target.value);
 });
 
@@ -22,14 +24,32 @@ tipPercentage.forEach((percentage) => {
   percentage.addEventListener("click", () => {
     if (percentage.textContent === "5%") {
       tipAmount.value = billInput.value * 0.05;
+      total.value = billInput.value * 0.05;
     } else if (percentage.textContent === "10%") {
       tipAmount.value = billInput.value * 0.1;
+      total.value = billInput.value * 0.1;
     } else if (percentage.textContent === "15%") {
       tipAmount.value = billInput.value * 0.15;
+      total.value = billInput.value * 0.15;
     } else if (percentage.textContent === "25%") {
       tipAmount.value = billInput.value * 0.25;
+      total.value = billInput.value * 0.25;
     } else if (percentage.textContent === "50%") {
       tipAmount.value = billInput.value * 0.5;
+      total.value = billInput.value * 0.5;
     }
   });
+});
+
+peopleInput.addEventListener(
+  "click",
+  () => {
+    peopleInput.value = "";
+  },
+  { once: true }
+);
+
+peopleInput.addEventListener("input", (e) => {
+  peopleInput.value = e.target.value;
+  console.log(peopleInput.value);
 });

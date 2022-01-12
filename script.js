9a54b3f51155994e81dc10f582ae1a86b331ca73
Alan Pečenković <alanquad@yahoo.com>
2022-01-12 10:18:57 +0100

    input DOM elements selected correctly

---
+++ script.js
@@ -0,0 +1,11 @@
+const billInput = document.querySelector('#bill');
+const tipPercentage = document.querySelectorAll('.number');
+const peopleInput = document.querySelector('#people');
+const tipAmount = document.querySelector('#tip-amount');
+const total = document.querySelector('#total');
+
+console.log(billInput);
+console.log(tipPercentage);
+console.log(peopleInput);
+console.log(tipAmount);
+console.log(total);

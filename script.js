5ff4e7b6c609e7dcd1e53fd25cfa746ad15fa6ff
Alan Pečenković <alanquad@yahoo.com>
2022-01-12 10:59:17 +0100

    added event listeners to billing input and percentage divs

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ const peopleInput = document.querySelector('#people');
 const tipAmount = document.querySelector('#tip-amount');
 const total = document.querySelector('#total');
 
-console.log(billInput);
-console.log(tipPercentage);
-console.log(peopleInput);
-console.log(tipAmount);
-console.log(total);
+billInput.addEventListener('click', ()=> {
+    billInput.value = '';
+}, { once: true });
+
+billInput.addEventListener('input', (e) => {    
+    billInput.value = e.target.value;   
+    console.log(e.target.value);
+});
+
+tipPercentage.forEach(percentage => {
+    percentage.addEventListener('click', () => {
+        console.log(percentage);
+        console.log(billInput.value);
+    })
+})
+
+// console.log(billInput);
+// console.log(tipPercentage);
+// console.log(peopleInput);
+// console.log(tipAmount);
+// console.log(total);
